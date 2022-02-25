739af644b13606b55926f262e02a77a3b53ea591
KristianWede <kris79s4@stud.kea.dk>
2022-02-25 13:15:16 +0100

    Added greet function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    Scanner sc = new Scanner(System.in);
+
+    public void greet(){
+        System.out.println("Hej med dig! Hvad er dit navn?");
+        String navn = sc.nextLine();
+        System.out.println("Hej " + navn + "!");
+        System.out.println("Hej igen!");
+    }
+
     public static void main(String[] args) {
+        Main obj = new Main();
+
+        obj.greet();
         System.out.println("It works!");
     }
 }
